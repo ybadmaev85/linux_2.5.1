b41da995b51015235b2017e3434ba865b7ccf280
Victor <cool.badmaev@bk.ru>
2023-09-30 19:47:31 +0300

    ready dz 2(add filter)

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 from payment.models import Pay
 from payment.serializers import PaySerializer
 
@@ -7,5 +8,6 @@ from payment.serializers import PaySerializer
 class PayViewSet(viewsets.ModelViewSet):
     serializer_class = PaySerializer
     queryset = Pay.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ('payday', 'pay_method', 'is_paid')
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('is_paid', 'pay_method',)
+    ordering_fields = ('payday', )
